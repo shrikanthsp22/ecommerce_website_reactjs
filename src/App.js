7fe3a2583f5ef301434913f1db145c4b439c90dd
shrikanthsp22 <shrikanthsp22@gmail.com>
2019-09-22 12:43:16 +0530

    lesson6-firstcommit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {Switch,Route} from 'react-router-dom';
+import HomePage from '../src/pages/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE </h1>
+//   </div>
+// );
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
