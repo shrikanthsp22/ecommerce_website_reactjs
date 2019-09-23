42ecd35fafe4e83fe9638cbec95733e2b4493616
shrikanthsp22 <shrikanthsp22@gmail.com>
2019-09-23 18:30:39 +0530

    23rd

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,14 +10,14 @@ import './header.style.scss';
 const Header = () => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+    <Logo className='logo' />   
     </Link>
     <div className='options'>
       <Link className='option' to='/shop'>
-        Shop
+       SHOP
       </Link>
       <Link className='option' to='/shop'>
-        Contact Us
+        CONTACT US
       </Link>
     </div>
   </div>
